fe4e5303fba66c6bd0d0de8c1072b7da45d641fd
Obaro <edeas123@gmail.com>
2017-03-30 16:22:37 -0600

    update and optimize

--- src/python/centerline.py
+++ src/python/centerline.py
@@ -20,21 +20,13 @@ def get_points(floor, building):
     arc_path = data_path + os.sep + "arcgis" + os.sep + "campus.gdb" # set arc files directory
 
     arcpy.env.workspace = arc_path
-    feature_class = arc_path + os.sep + "CenterLine2_1mPoints"
+    feature_class = arc_path + os.sep + "CenterLine2_1mPoints_b"
     
-    rows = arcpy.SearchCursor(feature_class)
-    row = rows.next()
-    
-    fname_map = {"ground floor": 0, "1st floor": 1,  "2nd floor":2, "3rd floor": 3, "basement floor": -1,
-                 "4th floor": 4, "5th floor": 5, "6th floor": 6, "7th floor": 7,
-                 "8th floor": 8, "9th floor": 9, "10th floor": 10, "11th floor": 11,
-                 "12th floor": 12, "13th floor": 13, "14th floor": 14, "15th floor": 15,
-                 "lower floor": -1, "main floor": 1}
+    clause = '"bname"' +  " = '" + str(building) + "' and " + '"fnum"' +  " = " + str(floor)
     
     centerline_x_y = pd.DataFrame()
-    while row:
-        if (floor == fname_map[row.fname] and building==row.bname):
-            centerline_x_y = centerline_x_y.append(pd.DataFrame(data=[[row.X, row.Y]]), ignore_index=True)
-        row = rows.next()
-
+    with arcpy.da.SearchCursor(feature_class, ("fname", "bname", "x", "y",), clause) as cursor:
+        for row in cursor:
+            centerline_x_y = centerline_x_y.append(pd.DataFrame(data=[[row]]), ignore_index=True)
+            
     return centerline_x_y.values
